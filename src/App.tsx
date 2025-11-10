5536bff82441ffedb17195745df8d528df46a31a
Cecilia Montes <136648651+cimontesm@users.noreply.github.com>
2025-11-10 19:10:00 +0000

    guia 12 hasta grid

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Grid } from '@mui/material';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Grid>
+
+         {/* Encabezado */}
+         <Grid>Elemento: Encabezado</Grid>
+
+         {/* Alertas */}
+         <Grid>Elemento: Alertas</Grid>
+
+         {/* Selector */}
+         <Grid>Elemento: Selector</Grid>
+
+         {/* Indicadores */}
+         <Grid>Elemento: Indicadores</Grid>
+
+         {/* Gráfico */}
+         <Grid>Elemento: Gráfico</Grid>
+
+         {/* Tabla */}
+         <Grid>Elemento: Tabla</Grid>
+
+         {/* Información adicional */}
+         <Grid>Elemento: Información adicional</Grid>
+
+      </Grid>    
+  )
+}
+
+export default App
