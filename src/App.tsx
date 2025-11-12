6ab574b418fba91aad3c33831dac971bb72c2da4
Cecilia Montes <136648651+cimontesm@users.noreply.github.com>
2025-11-12 19:08:06 +0000

    guia 12 fin

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { Grid } from '@mui/material';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Grid container spacing={5} justifyContent="center" alignItems="center">
 
