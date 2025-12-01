469cb81816c9398a60ee4c7d8d9a223fc1b09360
Cecilia Montes <136648651+cimontesm@users.noreply.github.com>
2025-12-01 19:47:36 +0000

    guia 14

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Grid } from '@mui/material';
 import './App.css'
 import HeaderUI from './components/HeaderUI';
 import AlertUI from './components/AlertUI';
+import SelectorUI from './components/SelectorUI';
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <Grid size={{ xs: 12, md: 12 }}><AlertUI description="No se preveen lluvias" /></Grid>
 
       {/* Selector */}
-      <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
+      <Grid size={{ xs: 12, md: 3 }}><SelectorUI/></Grid>
 
       {/* Indicadores */}
       <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
